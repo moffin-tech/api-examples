44c27bf60a65b91482c9a08594bfea2162f96394
Victor Diaz <mars.vic5@gmail.com>
2022-05-26 19:01:21 -0600

    feat(prospector example): example of how to request a report from prospector

---
+++ request-prospector-pf/index.js
@@ -0,0 +1,34 @@
+const Axios = require('axios')
+require('dotenv').config()
+
+const client = Axios.create({
+  baseURL: 'https://sandbox.moffin.mx/api/v1',
+  headers: {
+    // You can create this token in https://sandbox.moffin.mx/configuracion/api
+    'Authorization': 'Token ' + process.env.API_TOKEN
+  }
+})
+
+async function start () {
+  const { data: profile } = await client.post('/profile', {
+    accountType: 'PF',
+    email: 'test-dev@moffin.mx',
+    firstName: 'UNO',
+    firstLastName: 'PRUEBA',
+    secondLastName: 'PROSPECTOR',
+    basicRFC: 'PRPU800101',
+    address: 'Jaime Balmes',
+    city: 'Cd de Mexico',
+    state: 'CMX',
+    zipCode: '11510',
+    exteriorNumber: '8',
+    neighborhood: 'Los morales',
+    country: 'MX',
+    nationality: 'MX'
+  })
+  const { data: serviceQueries } = await client.post(`/profiles/${profile.id}/query`, {
+    prospectorPF: true
+  })
+  const [prospectorRequest] = serviceQueries.results
+  console.log('Prospector request', serviceQuery)
+}
